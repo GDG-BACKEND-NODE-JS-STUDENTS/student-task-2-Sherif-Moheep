bc430aa838f06aa0f6c0c52be74aee3cbc8f1389
Sherif_Moheep <sherifmohep@gmail.com>
2025-03-09 03:19:03 +0200

    Tast 2 done

--- Assignment/tempCodeRunnerFile.js
+++ Assignment/tempCodeRunnerFile.js
@@ -1,6 +1,5 @@
 // Write a **script** that declares variables using `let`, `const`, and `var`.
 
-console.log("Variables using let, const, and var");
 var my_name = "Sherif";
 let my_age = 19;
 const pi = 3.14;
@@ -10,16 +9,13 @@ const pi = 3.14;
 
 let x = 5, y = 2;
 
-console.log("Basic operations using arithmetic operators");
 console.log(x + y); // 7
 console.log(x - y); // 3
 console.log(x * y); // 10
 console.log(x / y); // 2.5
 console.log(x % y); // 1
 console.log(x ** y); // 25 (5²)
-console.log("\n");
 
-console.log("Basic operations using Comparison Operators");
 console.log(7 == "7"); // true
 console.log(7 === "7"); // false
 console.log(7 != "7"); // false
@@ -28,20 +24,16 @@ console.log(5 > 7); // false
 console.log(5 < 7); // true
 console.log(5 >= 7); // false
 console.log(7 <= 7); // true
-console.log("\n");
 
-console.log("Basic operations using Logical Operators");
 console.log(1 < 2 && 2 < 3); // true
 console.log(1 < 2 && 2 > 3); // false
 console.log(1 < 2 || 2 > 3); // true
 console.log(1 > 2 || 2 > 3); // false
 console.log(!(1 > 2)); // true
-console.log("\n");
 
 
 // Practice **type conversion** (e.g., converting a number to a string).
 
-console.log("Type conversion");
 let num = 5;
-let num_to_str = String(num);
-console.log(num_to_str); // "5"
+let num_to_str = Number(num);
+console.log(num_to_str); // 5
